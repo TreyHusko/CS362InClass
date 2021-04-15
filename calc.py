c7a0d9f04716d97ca85f5c82b221328703da11cd
TreyHusko <58197079+TreyHusko@users.noreply.github.com>
2021-04-15 16:54:22 -0700

    Prints list

--- calc.py
+++ calc.py
@@ -1,3 +1,10 @@
 def calc(a, b):
     sum = a + b
-    print(sum)
+    diff = a - b 
+    multiply = a * b 
+    divide = a / b
+
+    resultsNum = [sum, diff, multiply, divide]
+    print(resultsNum)
+
+calc(4, 3)
