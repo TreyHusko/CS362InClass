2456ca9e4f58d55ded55fb3842d70b9124716d95
TreyHusko <58197079+TreyHusko@users.noreply.github.com>
2021-04-15 19:11:34 -0700

    added print statements/ user input

--- divisor.py
+++ divisor.py
@@ -1,10 +1,12 @@
 def divisor(d):
     i = 1
+    print("The divisors of " + str(d) + " are: ")
     while i <= d:
         if (d % i == 0):
             print(i)
         i += 1
-    
 def getNumber(prompt):
     userNum = int(input(prompt))
     return userNum
+
+input = getNumber("Enter a number to see the divisors")
