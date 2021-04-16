4cd42d6d9ab57a14a844e589c66b7fa63a2ebbd9
TreyHusko <58197079+TreyHusko@users.noreply.github.com>
2021-04-15 18:26:10 -0700

    Added code for divisor

--- divisor.py
+++ divisor.py
@@ -1,7 +1,10 @@
-def divisor(a):
-    break
-print
-
+def divisor(d):
+    i = 1
+    while i <= d:
+        if (d % i == 0):
+            print(i)
+        i += 1
+    
 def getNumber(prompt):
     userNum = int(input(prompt))
     return userNum
